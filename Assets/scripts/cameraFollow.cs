57078f68ce07127004c84acd74799186d9fa047f
Jonathan <jonathan.tippett@ubalt.edu>
2014-10-31 16:07:58 -0400

    Camera Follow
    Added a script for the camera to follow the player.

---
+++ Assets/scripts/cameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float distance = 1f;
+	public float offsetx = 3f;
+	public float offsety = 0f;
+	public float damping = 5f;
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 wantedPosition = target.TransformPoint (offsetx, offsety, -distance);
+		transform.position = Vector3.Lerp (transform.position, wantedPosition, Time.deltaTime * damping);
+	}
+}
